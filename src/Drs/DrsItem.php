7f4522fd1732cd46d7f9323c5d4ef74a67886fdc
Steven Bassett <steven.j.bassett@gmail.com>
2013-04-24 09:44:41 -0400

    Updated the permssions check on the DRS object.

--- src/Drs/DrsItem.php
+++ src/Drs/DrsItem.php
@@ -7,7 +7,11 @@ class DrsItem {
   public $pid;
   
 
-
+  /**
+   * Building the construct object for DRS items when they are created.
+   * @param string $pid  unique string in the DRS fedora repository.
+   * @param object $solr Solarium item class.
+   */
   public function __construct($pid, $solr){
     $this->pid = $pid;
     $query = $solr->createSelect();
@@ -18,8 +22,30 @@ class DrsItem {
     foreach($doc[0] as $key => $value){
       $this->$key = $value;
     }
+  }
+
+  /**
+   * Permission check on the object
+   * @param  string $permission The CRUD permission to check for the object.
+   * @param  string $identities The identity strings that are allowed to preform the CRUD operation.
+   * @return boolean             If they can perform that action.
+   */
+  public function permission_check($permission, $identities){
+    $permission = strtolower($permission);
+    $permarray = array('create','read','update','delete');
+    if (!in_array($permission, $permarray)){
+      return False;
+    }
+    else{
+      if($identities,$this->permission){
+        return True;
+      }
+      else{
+        return False;
+      }
+    }
+    
 
   }
-  
 
 }
